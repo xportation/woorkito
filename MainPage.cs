41e212aec404030ed841f3468867d77230783597
Leonardo Vitor da Silva <xportation@gmail.com>
2016-10-06 00:40:01 -0300

    Initial version with a basic tabed application. Only the iOS project at this moment.

---
+++ MainPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace woorkito
+{
+	public class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			Children.Add(buildNavigationPage(new RegisterPage(), "Register", "register.png"));
+			Children.Add(buildNavigationPage(new ReportPage(), "Report", "report.png"));
+			Children.Add(buildNavigationPage(new SettingsPage(), "Settings", "settings.png"));
+		}
+
+		private NavigationPage buildNavigationPage(ContentPage page, String title, String iconPath)
+		{
+			var navigationPage = new NavigationPage(page);
+			navigationPage.Title = title;
+			//navigationPage.Icon = iconPath;
+			return navigationPage;
+		}
+	}
+}
+
