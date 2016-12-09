da71a79654c7afc636a0febb804fff2954d21244
zapelini <ivanilsonz@gmail.com>
2016-12-09 02:27:44 -0200

    Create Android Project. Need to update realm to v0.80.0.0.

---
+++ Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using woorkito;
+
+namespace Woorkito.Android
+{
+    [Activity(Label = "Woorkito", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            ActionBar.SetIcon(null);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
